af4e4d34e40dbfd6164a1595a83311e606657d72
mwroblewski <milwroblewski@gmail.com>
2022-03-01 19:16:25 +0100

    Serializing and deserializing Employees to file

--- UITask.Common/DataProvider/EmployeeDataProvider.cs
+++ UITask.Common/DataProvider/EmployeeDataProvider.cs
@@ -1,23 +1,46 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
 
 namespace UITask.Common
 {
-    internal class EmployeeDataProvider : IEmployeeDataProvider
+    public class EmployeeDataProvider : IEmployeeDataProvider
     {
-        readonly List<Employee> _employees = new();
-
+        private readonly List<Employee> _employees = new();
+        private static readonly string fileName = "employees.json";
+        private readonly string path;
         public EmployeeDataProvider()
         {
-
+            path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), fileName);
+            if (File.Exists(path) == false)
+            {
+                File.Create(path).Close();
+            }
+            var fileContent = File.ReadAllText(path);
+            if (string.IsNullOrEmpty(fileContent) == false)
+            {
+                var deserializedEmployees = JsonSerializer.Deserialize(File.ReadAllText(path), typeof(List<Employee>));
+                _employees = deserializedEmployees as List<Employee>;
+            }
         }
         public IEnumerable<Employee> LoadEmployees()
         {
-            return null;
+            return _employees;
         }
 
         public void SaveEmployee(Employee who)
         {
             _employees.Add(who);
+            var serializedEmployees = JsonSerializer.SerializeToUtf8Bytes(_employees);
+            try
+            {
+                File.WriteAllBytes(path, serializedEmployees);
+            }
+            catch (Exception error)
+            {
+                //could use some log here
+            }
         }
     }
 }
