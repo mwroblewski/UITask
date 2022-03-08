505ae302afa8345514918263c0839bd2b9098567
mwroblewski <milwroblewski@gmail.com>
2022-03-08 21:16:18 +0100

    Views navigation added

--- UITask/MainWindow.xaml.cs
+++ UITask/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -37,6 +38,50 @@ namespace UITask
         private void LoadMainViewModelData(object _, WindowActivatedEventArgs unneeded)
         {
             ViewModel.Load();
+            NavigateToPage(AvailablePages[Views.NewEmployee]);
+        }
+
+        private void ChangeView(object sender, SelectionChangedEventArgs e)
+        {
+            Console.WriteLine((sender as ListView).SelectedValue);
+        }
+
+        private void nvSample5_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if ((args.SelectedItem as NavigationViewItem)?.Tag is not string selectedTag)
+            {
+                return;
+            }
+            var pageName = AvailablePages[(Views)Enum.Parse(typeof(Views), selectedTag)];
+            NavigateToPage(pageName);
+        }
+
+        private void NavigateToPage(string withName)
+        {
+            if (string.IsNullOrEmpty(withName))
+            {
+                //Could also throw or log
+                return;
+            }
+            var type = Type.GetType(withName);
+            if (type is null)
+            {
+                //Could also throw or log
+                return;
+            }
+            contentFrame.Navigate(type);
+        }
+
+        private static Dictionary<Views, string> AvailablePages => new()
+        {
+            { Views.NewEmployee, typeof(Pages.NewEmployee).FullName },
+            { Views.EmployeesList, typeof(Pages.EmployeesList).FullName }
+        };
+
+        private enum Views
+        {
+            NewEmployee,
+            EmployeesList
         }
     }
 }
