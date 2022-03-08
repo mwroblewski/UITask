c9e21a7acb66d27600686f1a33e07b11d9256347
mwroblewski <milwroblewski@gmail.com>
2022-03-08 21:38:20 +0100

    Cleanup in MainWindows class

--- UITask/MainWindow.xaml.cs
+++ UITask/MainWindow.xaml.cs
@@ -1,20 +1,7 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using UITask.Common;
-using UITask.Common.ViewModel;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,27 +13,25 @@ namespace UITask
     /// </summary>
     public sealed partial class MainWindow : Window
     {
-        public MainViewModel ViewModel { get; }
+        private static Dictionary<Views, string> AvailablePages => new()
+        {
+            { Views.NewEmployee, typeof(Pages.NewEmployee).FullName },
+            { Views.EmployeesList, typeof(Pages.EmployeesList).FullName }
+        };
+
+        private enum Views
+        {
+            NewEmployee,
+            EmployeesList
+        }
 
         public MainWindow()
         {
             this.InitializeComponent();
-            ViewModel = new MainViewModel(new EmployeeDataProvider());
-            Activated += LoadMainViewModelData;
+            Activated += (_, _) => { NavigateToPage(AvailablePages[Views.NewEmployee]); };
         }
 
-        private void LoadMainViewModelData(object _, WindowActivatedEventArgs unneeded)
-        {
-            ViewModel.Load();
-            NavigateToPage(AvailablePages[Views.NewEmployee]);
-        }
-
-        private void ChangeView(object sender, SelectionChangedEventArgs e)
-        {
-            Console.WriteLine((sender as ListView).SelectedValue);
-        }
-
-        private void nvSample5_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        private void ChangePage(NavigationView _, NavigationViewSelectionChangedEventArgs args)
         {
             if ((args.SelectedItem as NavigationViewItem)?.Tag is not string selectedTag)
             {
@@ -70,18 +55,6 @@ namespace UITask
                 return;
             }
             contentFrame.Navigate(type);
-        }
-
-        private static Dictionary<Views, string> AvailablePages => new()
-        {
-            { Views.NewEmployee, typeof(Pages.NewEmployee).FullName },
-            { Views.EmployeesList, typeof(Pages.EmployeesList).FullName }
-        };
-
-        private enum Views
-        {
-            NewEmployee,
-            EmployeesList
         }
     }
 }
