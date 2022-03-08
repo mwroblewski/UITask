c954603ffd473db06257a2496ec7dfbe2c62e601
mwroblewski <milwroblewski@gmail.com>
2022-03-08 20:36:35 +0100

    Added EmployeesListViewModel

---
+++ UITask.Common/ViewModel/EmployeesListViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UITask.Common.ViewModel
+{
+    public class EmployeesListViewModel
+    {
+        private readonly IEmployeeDataProvider _dataProvider;
+
+        public EmployeesListViewModel(IEmployeeDataProvider dataProvider)
+        {
+            _dataProvider = dataProvider;
+        }
+
+        public ObservableCollection<EmployeeViewModel> Employees { get; } = new();
+
+        public EmployeeViewModel SelectedEmployee { get; set; }
+
+        public void Load()
+        {
+            //TODO could put some watcher on the file 
+            var employees = _dataProvider.LoadEmployees();
+            //Employees.Clear();
+            foreach (var employee in employees)
+            {
+                Employees.Add(new EmployeeViewModel(employee, _dataProvider));
+            }
+        }
+    }
+}
